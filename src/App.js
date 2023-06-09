236672037717473ff3ee32b271991736aa85cebb
George Kutsia <Georgekutsia@gmail.com>
2023-06-09 12:28:30 +0100

    cambiado Build a Buil

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@ import { AuthContext } from './context/AuthContext';
 // import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 
 
-
-function AuthGuard() {
-}
-
 function App() {
   const connected = useContext(AuthContext)
-const [countMons, setCountMons] = useState(4)
 
   return (
     <div >
