4f1310ac0509045b85eb929a0def844f6a376055
George Kutsia <Georgekutsia@gmail.com>
2023-06-22 16:05:31 +0100

    contadores personajae

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
-import {NavBar} from './component';
-import { ShopScreen, RuleScreen, EventsScreen, DongionsDetail, FrontFrontpage, CounterScreen, PjScreen} from "./screens"
-
+import { NavBar } from './component';
+import { ShopScreen, RuleScreen, EventsScreen, DongionsDetail, FrontFrontpage, CounterScreen, PjScreen } from "./screens"
 
 function App() {
-  const [vida, setVida] = useState(0)
+  const [vida, setVida] = useState(0);
+  const [pjScreenVisible, setPjScreenVisible] = useState(false);
+
+  const togglePjScreen = () => {
+    setPjScreenVisible(!pjScreenVisible);
+  };
 
   return (
-    <div >
-    <NavBar/>
-    
-    <PjScreen/>
-          <div className="cur1" >
-            <Routes>
-              <Route path="/" element={<FrontFrontpage/>} />
-              <Route path="/eventos" element={<EventsScreen/>}/>
-              <Route path="/shop" element={<ShopScreen/>}/>
-              <Route path="/rules" element={<RuleScreen/>}/>
-              <Route path="/dongionsdDetail" element={<DongionsDetail/>}/>
-              <Route path="/counter" element={<CounterScreen/>}/>
-            </Routes>
-          </div>
+    <div>
+      <NavBar />
+
+      <button onClick={togglePjScreen}>
+        {pjScreenVisible ? "Ocultar PjScreen" : "Mostrar PjScreen"}
+      </button>
+      {pjScreenVisible && <PjScreen />}
+
+      <div className="cur1">
+        <Routes>
+          <Route path="/" element={<FrontFrontpage />} />
+          <Route path="/eventos" element={<EventsScreen />} />
+          <Route path="/shop" element={<ShopScreen />} />
+          <Route path="/rules" element={<RuleScreen />} />
+          <Route path="/dongionsdDetail" element={<DongionsDetail />} />
+          <Route path="/counter" element={<CounterScreen />} />
+        </Routes>
+      </div>
     </div>
   );
 }
