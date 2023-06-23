77cdc1824f1669cc7848da5b9fa3c83b791eaa16
George Kutsia <Georgekutsia@gmail.com>
2023-06-23 14:08:05 +0100

    mejor

--- src/component/creador/Creador.js
+++ src/component/creador/Creador.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Creador() {
   return (
-    <div className="justify-content-evenly box-steps-creator-rules "  >
+    <div className="box-steps-creator-rules "  >
           <div>Tanto los personajes como parte del lore pertenecen a "Wizard Of The Coast LLC, subsidiario de Hashbro, Inc. Todos los derechos reservados"</div>
           <div>Esta página web no está afiliada ni respaldada por ellos, es un proyecto personal no lucrativo</div>
           <div>Si Wizard Of The Coast LLC o algún otro artista o autor no quiere que use las imágenes, puede contactar conmigo en <i className="fa-solid fa-envelope"> dongionscreator@gmail.com</i> </div>
