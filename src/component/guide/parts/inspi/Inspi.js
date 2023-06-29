9cbaf7b342a92167bea8630470fc1e25c96ad31f
George Kutsia <Georgekutsia@gmail.com>
2023-06-29 01:39:13 +0100

    more inspi

--- src/component/guide/parts/inspi/Inspi.js
+++ src/component/guide/parts/inspi/Inspi.js
@@ -49,38 +49,53 @@ function Inspi() {
       </div>
       <div>
         <h4 onClick={()=>setins4(!ins4)} style={{color:"#ff8000"}} className={ins4 ? "inspi-titulo-click":"inspi-titulo"}> Defensor ofensivo</h4>
-      <img onClick={()=>setins4(!ins4)} className={ins4 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/meledoble_fc7j6p.png" alt="Inspi" />
+        <img onClick={()=>setins4(!ins4)} className={ins4 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/meledoble_fc7j6p.png" alt="Inspi" />
               {ins4 && 
-        <article className='artins4'> Un secreto que el gremio de los Defensores supo descubrir, es que un luchador que mitiga todo el bloqueo, puede devolver incluso el daño al atacante y con dos escudos encantados para beneficiarse entre sí, usar armas de filo es un capricho.
-        Un equipo pesado, dos escudos grandes y aspirar a la inmortalidad,  </article>
-        }
+                <>
+                  <article className='artins4'> Un secreto que el gremio de los Defensores supo descubrir, es que un luchador que mitiga todo el bloqueo, puede devolver incluso el daño al atacante y con dos escudos encantados para beneficiarse entre sí, usar armas de filo es un capricho.
+                  Un equipo pesado, dos escudos grandes y apego a la inmortalidad te volverá un tanque devastador  </article>
+                  <article className='artins4'> Usa habilidades de los escudos para devastar al enemigo a nivel físico y moral, si quieres arrójalos y destuye Golems gigantescos con un solo lanzamiento. ¿Quién necesita sanadores o luchadores siendo un arma indestructible?</article>
+                </>
+              }
       </div>
       <div>
         <h4 onClick={()=>setins5(!ins5)} style={{color:"#585656"}} className={ins5 ? "inspi-titulo-click":"inspi-titulo"}> Brutalidad Desbordante</h4>
       <img onClick={()=>setins5(!ins5)} className={ins5 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/bruto_q7jffm.png" alt="Inspi" />
               {ins5 && 
-        <article className='artins5'> Bulala </article>
-        }
+                <>
+                <article className='artins5'> En este mundo vasto y generoso se pueden conseguir armas sin dificultad, asi que lánzalos sin miramientos. Vuelve tus armas irrompibles con la rotura disminuída a 1. Al reequiparte, tienes ataque físico gratuito, así que lanza esas armas a cualquier cosa que se mueva que no debería moverse. </article>
+                <article> Imagina una arpía volando, inalcanzable con armas… ¡pues se las lanzas! El destrozo será inesperado, bajará de altura, lo que puedes aprovechar para sacar una maza y meterle una toña que la estampe a una pared. Luego puedes lanzarle la misma maza, corres y la empalas con una lanza. </article>
+                </>
+              }
       </div>
       <div>
         <h4 onClick={()=>setins6(!ins6)} style={{color:"#51ff00"}} className={ins6 ? "inspi-titulo-click":"inspi-titulo"}> Aparatero Cachivaches</h4>
       <img onClick={()=>setins6(!ins6)} className={ins6 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/cachivaches_omz3wx.png" alt="Inspi" />
               {ins6 && 
-        <article className='artins6'> Bulala </article>
-        }
+              <>
+                <article className='artins6'> Hay una cantidad de aparatos y cachivaches brutal que hacen todo tipo de cosas. <span style={{fontStyle:"italic", color:"green"}}> "Brazo radial", "Alas aerodinámicas", "Batería de Rayos", "Catapultador Magnetoeléctrico", "Brazalete Imantado", "Detonador Químico" o "Detonador Eléctrico", "Taza de cosas locas al azar dentro…" </span>¡¿Quién rayos necesita magia teniendo las ventajas de la tecnología!?  </article>
+                <article className='artins6'> Tras activar un aparato, necesitarás esperar que se enfríe, pero para eso puedes estar armado hasta los dientes <span style={{fontStyle:"italic"}}> (hasta parecer un cíborg)</span> de todo para ¡todo tipo de situaciones! </article>
+              </>
+            }
       </div>
       <div>
-        <h4 onClick={()=>setins7(!ins7)} style={{color:"#29caf3"}} className={ins7 ? "inspi-titulo-click":"inspi-titulo"}> Sanador Inmortal</h4>
+        <h4 onClick={()=>setins7(!ins7)} style={{color:"#29caf3"}} className={ins7 ? "inspi-titulo-click":"inspi-titulo"}> Consumidor Empedernido</h4>
       <img onClick={()=>setins7(!ins7)} className={ins7 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/medico_xtvl7d.png" alt="Inspi" />
               {ins7 && 
-        <article className='artins7'> Bulala </article>
+              <>
+                <article className='artins7'> Hay tantas pociones de todo tipo en Beleron como Explosivos. ¿No quieres depender de un sanador? <span style={{color:"yellow"}}>Pociones...</span> ¿Quires pasarte el día reventando grupos de enemigos como entretenimiento? <span style={{color:"chocolate"}}>Explosivos...</span>  Suministros ilimitados y fáciles de usar. </article>
+                <article className='artins7'> Las pociones pueden darte un chute temporal de habilidades físicas extraordinarias y los explosivos mantenerte en movimiento, generando daño en área. ¿Sabías que hay formas de crear pociones que al lanzar crean efectos explosivos en área y crear explosivos que generen incluso agujeros de gusano o desmaterialicen materia orgánica? </article>
+              </>
         }
       </div>
       <div>
         <h4 onClick={()=>setins8(!ins8)} style={{color:"#ddff00"}} className={ins8 ? "inspi-titulo-click":"inspi-titulo"}> Físico Imbatible</h4>
       <img onClick={()=>setins8(!ins8)} className={ins8 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/desarmado_u3xrgr.png" alt="Inspi" />
               {ins8 && 
-        <article className='artins8'> Bulala </article>
+              <>
+                <article className='artins8'> Si te parece demasiado fácil zanjar problemas con armas y prefieres usar las manos para dejar algunas cosas claras, Cinestesia es lo tuyo. Marcialidad te da un dado extra para esquivar y con esquivas críticas, puedes contraatacar. </article>
+                <article className='artins8'> Imagina cebarte con alguien que no entiende cómo está siendo derrotado a pase de puñetazos y patadas. ¿Te esquiva un puñetazo? Sigue rápidamente con una patada. ¿La bloquea? Derríbalo y quítale las armas. Con botas y guantes específicos para combate desarmado, serás temido y admirado al mismo tiempo</article>
+              </>
         }
       </div>
       <div>
@@ -88,7 +103,27 @@ function Inspi() {
       <img onClick={()=>setins9(!ins9)} className={ins9 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687874174/donGions%20imgs/rules%2B/ninja_vzbzkj.png" alt="Inspi" />
               {ins9 && 
                 <article className='artins9'>Si quieres además, optar por ser apoyo de tu grupo usando Frases inspiradoras que tienen efectos devastadores, tanto de ayuda como detrimento, el mundo está lleno de páginas que invocarán todo tipo de hechizos.</article>
-
+        }
+      </div>
+      <div>
+        <h4 onClick={()=>setins9(!ins9)} style={{color:"#0095ff"}} className={ins9 ? "inspi-titulo-click":"inspi-titulo"}> Picaro Pendenciero</h4>
+      <img onClick={()=>setins9(!ins9)} className={ins9 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687998914/donGions%20imgs/rules%2B/mutar_m8kt1q.png" alt="Inspi" />
+              {ins9 && 
+                <article className='artins9'>Si quieres además, optar por ser apoyo de tu grupo usando Frases inspiradoras que tienen efectos devastadores, tanto de ayuda como detrimento, el mundo está lleno de páginas que invocarán todo tipo de hechizos.</article>
+        }
+      </div>
+      <div>
+        <h4 onClick={()=>setins9(!ins9)} style={{color:"#0095ff"}} className={ins9 ? "inspi-titulo-click":"inspi-titulo"}> Picaro Pendenciero</h4>
+      <img onClick={()=>setins9(!ins9)} className={ins9 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687999082/donGions%20imgs/rules%2B/libros1_lec8o4.png" alt="Inspi" />
+              {ins9 && 
+                <article className='artins9'>Si quieres además, optar por ser apoyo de tu grupo usando Frases inspiradoras que tienen efectos devastadores, tanto de ayuda como detrimento, el mundo está lleno de páginas que invocarán todo tipo de hechizos.</article>
+        }
+      </div>
+      <div>
+        <h4 onClick={()=>setins9(!ins9)} style={{color:"#0095ff"}} className={ins9 ? "inspi-titulo-click":"inspi-titulo"}> Picaro Pendenciero</h4>
+      <img onClick={()=>setins9(!ins9)} className={ins9 ? "inspi-img":"inspi-img-click"} src="https://res.cloudinary.com/dfrda73uc/image/upload/v1687998914/donGions%20imgs/rules%2B/barddo_xzjg6s.png" alt="Inspi" />
+              {ins9 && 
+                <article className='artins9'>Si quieres además, optar por ser apoyo de tu grupo usando Frases inspiradoras que tienen efectos devastadores, tanto de ayuda como detrimento, el mundo está lleno de páginas que invocarán todo tipo de hechizos.</article>
         }
       </div>
     </div>
