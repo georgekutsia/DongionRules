6a7c484d1943de8221ee5409a6cad07805593110
George Kutsia <Georgekutsia@gmail.com>
2023-07-01 00:40:51 +0100

    3x3

--- src/screens/map/MapScreen.js
+++ src/screens/map/MapScreen.js
@@ -25,25 +25,26 @@ function MapScreen({tamaño}) {
         <img src={`${images[0]}`} alt={images[0]} />
         <img src={`${images[1]}`} alt={images[1]} />
         <img src={`${images[2]}`} alt={images[2]} />
-        <img src={`${images[12]}`} alt={images[12]} />
+        {images[12] && <img src={`${images[12]}`} alt={images[12]} />}
       </div>
       <div>
         <img src={`${images[3]}`} alt={images[3]} />
         <img src={`${images[4]}`} alt={images[4]} />
         <img src={`${images[5]}`} alt={images[5]} />
-        <img src={`${images[13]}`} alt={images[13]} />
+        {images[13] && <img src={`${images[13]}`} alt={images[13]} />}
       </div>
       <div>
         <img src={`${images[6]}`} alt={images[6]} />
         <img src={`${images[7]}`} alt={images[7]} />
         <img src={`${images[8]}`} alt={images[8]} />
-        <img src={`${images[14]}`} alt={images[14]} />
+        {images[14] &&<img src={`${images[14]}`} alt={images[14]} />}
       </div>
       <div>
-      <img src={`${images[9]}`} alt={images[9]} />
-      <img src={`${images[10]}`} alt={images[10]} />
-      <img src={`${images[11]}`} alt={images[11]} />
-      <img src={`${images[15]}`} alt={images[15]} />
+      {images[9] &&<img src={`${images[9]}`} alt={images[9]} />}
+      {images[10] &&<img src={`${images[10]}`} alt={images[10]} />}
+      {images[11] &&<img src={`${images[11]}`} alt={images[11]} />}
+      {images[12] &&<img src={`${images[12]}`} alt={images[12]} />}
+
       </div>
     </div>
   );
