4d62e446430d4e17d2db2d82286bb80c97bb7af6
George <georgekutsia@gmail.com>
2024-07-29 21:19:36 +0100

    talentos

--- src/component/guide/talents/talentTree/TalentTreeButtonComponent.js
+++ src/component/guide/talents/talentTree/TalentTreeButtonComponent.js
@@ -16,15 +16,15 @@ function TalentTreeButtonComponent({ points, nombre, vertical, horizontal, empty
   };
   return (
     <>
-      {nombre && 
+      {nombre && !horizontal &&  !vertical && !empty &&
         <button style={{ width: `${btnWidth}`, height: `${btnHeight}` }} onClick={handleClick} 
         className={selected ? "talent-tree-btn talent-selected" : `${talentToSelect} talent-tree-btn ${btn}`}>{nombre}</button>
       }
       {horizontal &&
-        <button style={{ width: `${btnWidth}`, height: `${btnHeight}` }}  className='talent-tree-btn-line-hor'></button>
+        <button className={ selected ? `talent-tree-btn-line-hor talent-tree-filled-hor` : `talent-tree-btn-line-hor`}></button>
       }
       {vertical &&
-        <button style={{ width: `${btnWidth}`, height: `${btnHeight}` }}  className={ selected ? `talent-tree-btn-line-ver talent-tree-filled-ver` : `talent-tree-btn-line-ver`}></button>
+        <button className={ selected ? `talent-tree-btn-line-ver talent-tree-filled-ver` : `talent-tree-btn-line-ver`}></button>
       }
       {empty  && 
         <button style={{ width: `${btnWidth}`, height: `${btnHeight}` }} className='talent-tree-btn-space'></button>
--- src/component/guide/talents/talentTree/ajani/TalentTreeAjaniLuchador.js
+++ src/component/guide/talents/talentTree/ajani/TalentTreeAjaniLuchador.js
@@ -1,47 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import TalentTreeButtonComponent from '../TalentTreeButtonComponent'
-import {dataTalentsTreePoints} from '../../../../../data/dataTalentTreePoints'
-// nombre => texto del hechizo
-// nombre === empty  => crea cuadrícula vacía
-// nombre vacío creará una lína 
-// btn => color de los botones para saber de qué rama es
-// lineHor y lineVer para hacer lineaa verticales y horizontales entre botones
+import React, { useEffect, useState } from 'react';
+import TalentTreeButtonComponent from '../TalentTreeButtonComponent';
+import { dataTalentsTreePoints } from '../../../../../data/dataTalentTreePoints';
 
-function TalentTreeAjaniLuchador({}) {
-  const [pointsData, setPointsData] = useState(dataTalentsTreePoints)
-
+function TalentTreeAjaniLuchador() {
+  const [pointsData, setPointsData] = useState(dataTalentsTreePoints);
 
   return (
-      <div  className='talent-tree-box'>
+    <div className='talent-tree-box'>
       <div className='talent-tree-row'>
-        <TalentTreeButtonComponent  empty  btnWidth={"100%"}/>
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[1]} nombre={"+2 en Esquiva"} btnHeight={"60px"} btn={"btn-luchador"} talentToSelect={"talent-to-select "}/>
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[1]}  horizontal  btnWidth={"400px"}/>
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[2]} nombre={"+3 en Esquiva"} btnHeight={"60px"} btn={"btn-luchador"} talentToSelect={"talent-to-select "}/>
-        <TalentTreeButtonComponent  empty   btnWidth={"10%"}/>
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[3]} nombre={"Si dañas dos veces seguidas a un jugador, te come el culo"} 
-        btnWidth={"450%"} btnHeight={"80px"} btn={"btn-luchador"} talentToSelect={"talent-to-select "}/>
-      <TalentTreeButtonComponent  empty  btnWidth={"40%"}/>
+        <TalentTreeButtonComponent empty btnWidth={"100%"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[1]} nombre={"+2 en Esquiva"} btnHeight={"60px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[1]} horizontal btnWidth={"2400px"} nombre={"+3 en Esquiva"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[2]} nombre={"+3 en Esquiva"} btnHeight={"60px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
+        <TalentTreeButtonComponent empty btnWidth={"10%"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[3]} nombre={"Si dañas dos veces seguidas a un jugador, te come el culo"} btnWidth={"450%"} btnHeight={"80px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
+        <TalentTreeButtonComponent empty btnWidth={"40%"} />
       </div>
 
       <div className='talent-tree-row'>
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[1]} vertical />
-        <TalentTreeButtonComponent   empty />
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[2]} vertical />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[1]} vertical nombre={"+2 en Esquiva"} />
+        <TalentTreeButtonComponent empty />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[2]} vertical />
       </div>
-  
+
       <div className='talent-tree-row'>
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[0]} nombre={"Ha"} />
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[0]} vertical />
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[0]} nombre={"hihi"} />
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[0]} empty />
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[0]} empty />
-        <TalentTreeButtonComponent  points={pointsData.ajani.luchador[0]} nombre={"hay"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[0]} nombre={"Ha"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[0]} vertical />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[0]} nombre={"hihi"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[0]} empty />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[0]} empty />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[0]} nombre={"hay"} />
       </div>
+    </div>
+  );
+}
 
-      </div>
-    )
-  }
-  
-
-export default TalentTreeAjaniLuchador
+export default TalentTreeAjaniLuchador;
