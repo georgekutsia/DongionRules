4f9be0d63cfe36331da86baca5ce0c640aae7102
George <georgekutsia@gmail.com>
2024-07-29 21:41:20 +0100

    a ver

--- src/component/guide/talents/talentTree/ajani/TalentTreeAjaniLuchador.js
+++ src/component/guide/talents/talentTree/ajani/TalentTreeAjaniLuchador.js
@@ -13,13 +13,13 @@ function TalentTreeAjaniLuchador() {
   return (
     <div className='talent-tree-box'>
       <div className='talent-tree-row'>
-        <TalentTreeButtonComponent empty btnWidth={"15%"} />
-        <TalentTreeButtonComponent points={pointsData.ajani.luchador[1]} nombre={"+2 en Esquiva"} btnWidth={"550px"} btnHeight={"40px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
+        <TalentTreeButtonComponent empty btnWidth={"5%"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[1]} nombre={"+2 en Esquiva"} btnWidth={"350px"} btnHeight={"40px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
         <TalentTreeButtonComponent points={pointsData.ajani.luchador[1]} horizontal nombre={"+3 en Esquiva"} />
-        <TalentTreeButtonComponent points={pointsData.ajani.luchador[2]} nombre={"+3 en Esquiva"} btnWidth={"550px"} btnHeight={"40px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[2]} nombre={"+3 en Esquiva"} btnWidth={"350px"} btnHeight={"40px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
         <TalentTreeButtonComponent empty btnWidth={"10%"} />
-        <TalentTreeButtonComponent points={pointsData.ajani.luchador[3]} nombre={"AL dañas dos veces seguidas a un jugador, te come el culo"} btnWidth={"1200px"} btnHeight={"60px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
-        <TalentTreeButtonComponent empty btnWidth={"40%"} />
+        <TalentTreeButtonComponent points={pointsData.ajani.luchador[3]} nombre={"AL dañas dos veces seguidas a un jugador, te come el culo"} btnWidth={"500px"} btnHeight={"60px"} btn={"btn-luchador"} talentToSelect={"talent-to-select"} />
+        <TalentTreeButtonComponent empty btnWidth={"10%"} />
       </div>
 
       <div className='talent-tree-row'>
